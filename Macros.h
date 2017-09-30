64c23777b502be180229957d8815a4eea30ada2a
吉田大輝 <bob181219@yahoo.co.jp>
2017-09-30 14:10:08 +0900

    Describe Macros.h

---
+++ Macros.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// main & Login
+#define USERNAME_MAX 20
+#define PASSWORD_MAX 100
+
+// PrintVersion
+#define VERSION "1.0"
+
+// CommandLine & CommandProcess
+#define COMMAND_MAX 50
+
+// CommandProcess
+#define FILE_LINE_MAX 1000
